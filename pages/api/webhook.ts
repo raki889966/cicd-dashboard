80bbf2b2201c5273187e8e8bdae812fd42969fd0
Rakesh <kamathamrakesh7@gmail.com>
2025-06-20 19:25:53 -0500

    updated roots

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { saveBuild } from '../../lib/builds';
+import { saveBuild } from '../lib/builds';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
